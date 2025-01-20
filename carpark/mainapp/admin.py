f9cdd3869d7085733cc55166d907716313a54b82
Anardest <associalgamer@gmail.com>
2025-01-20 21:38:25 +0300

    feat: Добавлено новое приложение mainapp feat: Добавлена первая страница index.html

--- carpark/mainapp/admin.py
+++ carpark/mainapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Car, Driver, Trip
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'make', 'model_name', 'year')
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'surname', 'date_of_birth')
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('id', 'driver_id', 'car_id', 'distance')
+
--- carpark/mainapp/models.py
+++ carpark/mainapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+#TODO: car, driver, trip
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model_name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    mileage = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model_name}"
+
+class Driver(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    experience = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.date_of_birth}"
+
+class Trip(models.Model):
+    car_id = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True, blank=True)
+    driver_id = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True, blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    distance = models.FloatField()
+
+    def __str__(self):
+        return f"Поездка с {self.driver_id} в {self.car_id} с {self.start_time} по {self.end_time}"
